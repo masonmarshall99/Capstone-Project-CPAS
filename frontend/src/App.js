c5c8ad56c27766c288eedfa26a41c715f95ca5aa
Ivan <Ivan.Bezuidenhout2@gmail.com>
2025-08-04 21:45:22 +0800

    Revert "Base Paging Setup"
    This reverts commit 88c7491b74002acc77e6352a189047a9cdc5579c.

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,17 +1,19 @@
-import React from 'react';
-import { BrowserRouter as Router, Routes, Route, Navigate } from 'react-router-dom';
-import Dash from './Pages/Dash';
-import Account from './Pages/Account';
+import 'bulma/css/bulma.min.css';
+import React, { useEffect, useState } from 'react';
 
 function App() {
+  // For testing, remove in production
+  const [message, setMessage] = useState('');
+
+  useEffect(() => {
+    fetch('http://localhost:8000/api/test').then((res) => res.text()).then((data) => setMessage(data));
+  }, []);
+  // End of test
+
   return (
-     <Router>
-      <Routes>
-        <Route path="/" element={<Navigate to="/dash" replace />} />
-        <Route path="/dash" element={<Dash />} />
-        <Route path="/account" element={<Account />} />
-      </Routes>
-    </Router>
+    <div>
+      <p class="title">{message}</p>
+    </div>
   );
 }
 
