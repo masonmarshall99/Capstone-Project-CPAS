1e1e794f46abc903faf67c5631c8d3ff87870e53
Lachlan Buckley <lachlan.buckley04@gmail.com>
2025-08-05 20:32:49 +0800

    Begginning of model writing

--- backend/CPAS_Main/models.py
+++ backend/CPAS_Main/models.py
@@ -1,3 +1,22 @@
 from django.db import models
 
 # Create your models here.
+
+class Zone(models.Model):
+    zone_name = models.CharField(max_length=255, primary_key = True)
+
+class Location(models.Model):
+    sub_region = models.CharField(max_length=255, primary_key = True)
+    region = models.CharField(max_length=255)
+
+class Season(models.Model):
+    year = models.PositiveIntegerField(primary_key = True)
+
+class Crops(models.Model):
+    crop_name = models.CharField(max_length=255, primary_key = True)
+
+class CropArea(models.Model):
+    year = models.PositiveIntegerField(primary_key = True)
+    area_hectares = models.FloatField()
+    value_tonnes = models.FloatField()
+    # Fill in after confirmation regarding primary/foreign key
