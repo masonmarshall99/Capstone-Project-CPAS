920b3c1329366ac13f7a258f01a9952ff55e375d
Mason Marshall <mason.998@hotmail.com>
2025-09-04 14:32:48 +0800

    Setup inputs for Disease Impact Estimates, Treatment Cost Estimates and Harvest Estimates

--- frontend/src/Pages/Disease.js
+++ frontend/src/Pages/Disease.js
@@ -21,6 +21,23 @@ function Disease() {
   const [area, setArea] = useState(null)
   const [yieldRate, setYieldRate] = useState(null)
 
+  // Inputs for Disease Impact Estimates
+  const [disease, setDisease] = useState(null)
+  const [incidence, setIncidence] = useState(50)
+  const [areaOfEffect, setAreaOfEffect] = useState(50)
+  const [severity, setSeverity] = useState(50)
+
+  // Inputs for Treatment Cost Estimates
+  const [seedCost, setSeedCost] = useState(null)
+  const [firstApplicationCost, setFirstApplicationCost] = useState(null)
+  const [secondApplicationCost, setSecondApplicationCost] = useState(null)
+  const [otherOperationCost, setOtherApplicationCost] = useState(null)
+
+  // Inputs for Harvest Estimates
+  const [price, setPrice] = useState(null)
+  const [downgradePrice, setDowngradePrice] = useState(null)
+  const [yieldLoss, setYieldLoss] = useState(50)
+
   return (
     <>
       <title>Disease Risk</title>
@@ -42,10 +59,25 @@ function Disease() {
             setYieldRate={setYieldRate}/>
           </div>
           <div style={{display: "flex"}}>
-            <DiseaseImpactEstimates />
-            <TreatmentCostEstimates />
+            <DiseaseImpactEstimates 
+            setDisease={setDisease}
+            incidence={incidence}
+            setIncidence={setIncidence}
+            areaOfEffect={areaOfEffect}
+            setAreaOfEffect={setAreaOfEffect}
+            severity={severity}
+            setSeverity={setSeverity}/>
+            <TreatmentCostEstimates 
+            setSeedCost={setSeedCost}
+            setFirstApplicationCost={setFirstApplicationCost}
+            setSecondApplicationCost={setSecondApplicationCost}
+            setOtherApplicationCost={setOtherApplicationCost}/>
           </div>
-          <HarvestEstimates />
+          <HarvestEstimates 
+          setPrice={setPrice}
+          setDowngradePrice={setDowngradePrice}
+          yieldLoss={yieldLoss}
+          setYieldLoss={setYieldLoss}/>
           <CostBenefitSummary />
         </div>
       </div>
@@ -81,10 +113,10 @@ function RegionCropSeason({setRegion, setSubregion, setCrop, setSeason}) {
     <div style={{flex: 1, padding: "1rem"}}>
       <h1>Region, Crop & Season</h1>
       <div style={{flex: 1, padding: "1rem", border: "1px solid cyan", borderRadius: '10px'}}>
-        <div style={{display: "flex", gap: "1rem"}}>
+        <div style={{display: "flex", gap: "1rem", justifyContent: "space-evenly"}}>
           
           {/* Region Select */}
-          <div><label>
+          <span><label>
             <p>Region</p>
             <select defaultValue="" onChange={handleRegionChange}>
               <option value="" disabled hidden>Select Region</option>
@@ -92,10 +124,10 @@ function RegionCropSeason({setRegion, setSubregion, setCrop, setSeason}) {
                 <option value={region}>{region}</option>
               ))}
             </select>
-          </label></div>
+          </label></span>
 
           {/* Subregion Select */}
-          <div><label>
+          <span><label>
               <p>Subregion</p>
               <select defaultValue="" onChange={handleSubregionChange}>
               <option value="" disabled hidden>Select Subregion</option>
@@ -103,10 +135,10 @@ function RegionCropSeason({setRegion, setSubregion, setCrop, setSeason}) {
                 <option value={subregion}>{subregion}</option>
               ))}
             </select>
-          </label></div>
+          </label></span>
           
           {/* Crop Select */}
-          <div>
+          <span>
             <p>Crop</p>
             {cropList.map((crop) => (
               <label><p>
@@ -114,10 +146,10 @@ function RegionCropSeason({setRegion, setSubregion, setCrop, setSeason}) {
                 {crop}
               </p></label>
             ))}
-          </div>
+          </span>
 
           {/* Season Select */}
-          <div><label>
+          <span><label>
               <p>Season</p>
               <select defaultValue="" onChange={handleSeasonChange}>
               <option value="" disabled hidden>Select Season</option>
@@ -125,7 +157,8 @@ function RegionCropSeason({setRegion, setSubregion, setCrop, setSeason}) {
                 <option value={season}>{season}</option>
               ))}
             </select>
-          </label></div>
+          </label></span>
+
         </div>
       </div>
     </div>
@@ -134,11 +167,11 @@ function RegionCropSeason({setRegion, setSubregion, setCrop, setSeason}) {
 
 function CropProduction({area, setArea, yieldRate, setYieldRate}) {
 
-  const onAreaChange = (event) => {
+  const handleAreaChange = (event) => {
     setArea(event.target.value)
   }
 
-  const onYieldChange = (event) => {
+  const handleYieldChange = (event) => {
     setYieldRate(event.target.value)
   }
 
@@ -146,56 +179,214 @@ function CropProduction({area, setArea, yieldRate, setYieldRate}) {
     <div style={{flex: 1, padding: "1rem"}}>
       <h1>Crop Production</h1>
       <div style={{flex: 1, padding: "1rem", border: "1px solid cyan", borderRadius: '10px'}}>
-        <div style={{display: "flex", gap: "1rem"}}>
-            <div>
+        <div style={{display: "flex", gap: "1rem", justifyContent: "space-evenly"}}>
+            
+            {/* Planted Area Input */}
+            <span>
               <label>
                 <p>Planted Area</p>
-                <input type="number" style={{width: "50%"}} onChange={onAreaChange}/> hectares
+                <input type="number" style={{width: "50%"}} onChange={handleAreaChange}/> hectares
               </label>
-            </div>
-            <div>
+            </span>
+            
+            {/* Expected Yield Potential Input */}
+            <span>
               <label>
                 <p>Expected Yield Potential</p>
-                <input type="number" style={{width: "50%"}} onChange={onYieldChange}/> t/ha
+                <input type="number" style={{width: "50%"}} onChange={handleYieldChange}/> t/ha
               </label>
-            </div>
+            </span>
         </div>
+
+        {/* Expected Yield Display */}
         <div style={{padding: "1rem 0rem"}}>
           <h2>Expected yield: {Math.round(area * yieldRate * 1000)/1000} tonnes</h2>
         </div>
+
       </div>
     </div>
   );
 }
 
-function DiseaseImpactEstimates() {
+function DiseaseImpactEstimates({setDisease, incidence, setIncidence, areaOfEffect, setAreaOfEffect, severity, setSeverity}) {
+  const diseaseList = ["A", "B", "C"] // TODO: Query for disease
+
+  const handleDiseaseChange = (event) => {
+    setDisease(event.target.value)
+  }
+
+  const handleIncidenceChange = (event) => {
+    setIncidence(event.target.value)
+  }
+
+  const handleAreaChange = (event) => {
+    setAreaOfEffect(event.target.value)
+  }
+
+  const handleSeverityChange = (event) => {
+    setSeverity(event.target.value)
+  }
+
   return(
     <div style={{flex: 1, padding: "1rem"}}>
       <h1>Disease Impact Estimates</h1>
       <div style={{flex: 1, padding: "1rem", border: "1px solid cyan", borderRadius: '10px'}}>
-        Text
+        <div style={{display: "flex", gap: "1rem", justifyContent: "space-evenly"}}>
+          
+          {/* Disease Select */}
+          <span>
+            <label>
+              <p>Select disease present</p>
+              <select defaultValue="" onChange={handleDiseaseChange}>
+                <option value="" disabled hidden>Select Disease</option>
+                {diseaseList.map((disease) => (
+                  <option value={disease}>{disease}</option>
+                ))}
+              </select>
+            </label>
+          </span>
+
+          {/* Disease Incidince Input */}
+          <span>
+            <label>
+              <p>Disease Incidence (%)</p>
+              <input type="range" min="0" max="100" onChange={handleIncidenceChange}/>
+              <output>{incidence}</output>
+            </label>
+          </span>
+
+        </div>
+        <div style={{display: "flex", gap: "1rem", justifyContent: "space-evenly"}}>
+
+          {/* Disease Area Input */}
+          <span>
+            <label>
+              <p>Affected Area (%)</p>
+              <input type="range" min="0" max="100" onChange={handleAreaChange}/>
+              <output>{areaOfEffect}</output>
+            </label>
+          </span>
+
+          {/* Disease Severity */}
+          <span>
+            <label>
+              <p>Disease Severity (%)</p>
+              <input type="range" min="0" max="100" onChange={handleSeverityChange}/>
+              <output>{severity}</output>
+            </label>
+          </span>
+
+        </div>
       </div>
     </div>
   );
 }
 
-function TreatmentCostEstimates() {
+function TreatmentCostEstimates({setSeedCost, setFirstApplicationCost, setSecondApplicationCost, setOtherApplicationCost}) {
+
+  const handleSeedChange = (event) => {
+    setSeedCost(event.target.value)
+  }
+
+  const handleFirstApplicationChange = (event) => {
+    setFirstApplicationCost(event.target.value)
+  }
+
+  const handleSecondApplicationChange = (event) => {
+    setSecondApplicationCost(event.target.value)
+  }
+
+  const handleOtherApplicationChange = (event) => {
+    setOtherApplicationCost(event.target.value)
+  }
+
   return(
     <div style={{flex: 1, padding: "1rem"}}>
       <h1>Treatment Cost Estimates</h1>
       <div style={{flex: 1, padding: "1rem", border: "1px solid cyan", borderRadius: '10px'}}>
-        Text
+        <div style={{display: "flex", gap: "1rem", justifyContent: "space-evenly"}}>
+          
+          {/* Seed Cost */}
+          <span><label>
+            <p>Seed/In-furrow Cost ($/ha)</p>
+            $<input type="number" min="0" style={{width: "80%"}} onChange={handleSeedChange}/>
+          </label></span>
+
+          {/* First Application Cost */}
+          <span><label>
+            <p>First Application Cost ($/ha)</p>
+            $<input type="number" min="0" style={{width: "80%"}} onChange={handleFirstApplicationChange}/>
+          </label></span>
+
+        </div>
+        <div style={{display: "flex", gap: "1rem", justifyContent: "space-evenly"}}>
+          
+          {/* Second Application Cost */}
+          <span><label>
+            <p>Second Application Cost ($/ha)</p>
+            $<input type="number" min="0" style={{width: "80%"}} onChange={handleSecondApplicationChange}/>
+          </label></span>
+
+          {/* Other Operation Cost */}
+          <span><label>
+            <p>Other Operation Cost ($/ha)</p>
+            $<input type="number" min="0" style={{width: "80%"}} onChange={handleOtherApplicationChange}/>
+          </label></span>
+
+        </div>
       </div>
     </div>
   );
 }
 
-function HarvestEstimates() {
+function HarvestEstimates({setPrice, setDowngradePrice, yieldLoss, setYieldLoss}) {
+  
+  const handlePriceChange = (event) => {
+    setPrice(event.target.value)
+  }
+
+  const handleDowngradePriceChange = (event) => {
+    setDowngradePrice(event.target.value)
+  }
+
+  const handleYieldLossChange = (event) => {
+    setYieldLoss(event.target.value)
+  }
+
   return(
     <div style={{flex: 1, padding: "1rem"}}>
       <h1>Harvest Estimates</h1>
       <div style={{flex: 1, padding: "1rem", border: "1px solid cyan", borderRadius: '10px'}}>
-        Text
+        <div style={{display: "flex", gap: "1rem", justifyContent: "space-evenly"}}>
+          
+          {/* Market Price */}
+          <span><label>
+            <p>Market Price ($/t)</p>
+            $<input type="number" min="0" style={{width: "80%"}} onChange={handlePriceChange}/>
+          </label></span>
+
+          {/* Downgraded Price */}
+          <span><label>
+            <p>Downgraded Price ($/t)</p>
+            $<input type="number" min="0" style={{width: "80%"}} onChange={handleDowngradePriceChange}/>
+          </label></span>
+
+        </div>
+        <div style={{display: "flex", gap: "1rem", justifyContent: "space-evenly"}}>
+
+          {/* Yield Loss */}
+          <span><label>
+          <p>Proportion of yield loss to quality downgrade (%)</p>
+              <input type="range" min="0" max="100" onChange={handleYieldLossChange}/>
+              <output>{yieldLoss}</output>
+          </label></span>
+
+          {/* Average Price Display */}
+          <span><label>
+          {/* TODO: Investigate the average price statistic used in Harvest Stats for original prototype */}
+          </label></span>
+
+        </div>
       </div>
     </div>
   );
