bc6967637be6eeaf0aea38225e8f2fa8386d6a3f
Simon Zhang <simonzhang187@gmail.com>
2025-09-30 13:56:38 +0800

    WIP: changes in urls.py

--- backend/CPAS_Main/urls.py
+++ backend/CPAS_Main/urls.py
@@ -2,7 +2,7 @@ from django.urls import path
 from .views import create_user, logout_user, whoami
 
 urlpatterns = [
-    path("test/", test),
+    #path("test/", test),
     path("register/", create_user),
     path("logout/", logout_user),
     path("whoami/", whoami),
