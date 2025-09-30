77359774e1fa249a855ad303901e6539a086ec23
Simon Zhang <simonzhang187@gmail.com>
2025-09-30 15:30:23 +0800

    login user

--- backend/CPAS_Main/urls.py
+++ backend/CPAS_Main/urls.py
@@ -1,9 +1,10 @@
 from django.urls import path
-from .views import create_user, logout_user, whoami
+from .views import create_user, login_user, logout_user, whoami
 
 urlpatterns = [
     #path("test/", test),
     path("register/", create_user),
+    path("login/", login_user),
     path("logout/", logout_user),
     path("whoami/", whoami),
 ]
