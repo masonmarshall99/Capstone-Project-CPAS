8a164df3683cc49551fd567b4ec1319ccad7ec9a
lachlan-buckley <lachlan.buckley04@gmail.com>
2025-10-04 10:38:45 +0800

    conflict resolved

--- backend/CPAS_Main/urls.py
+++ backend/CPAS_Main/urls.py
@@ -1,8 +1,10 @@
 from django.urls import path
-from .views import create_user, logout_user, whoami, get_csrf_token, edit_user
+from .views import create_user, logout_user, whoami, get_csrf_token, edit_user, login_user
 
 urlpatterns = [
+    #path("test/", test),
     path("register/", create_user),
+    path("login/", login_user),
     path("logout/", logout_user),
     path("whoami/", whoami),
     path("get-csrf-token/", get_csrf_token),
