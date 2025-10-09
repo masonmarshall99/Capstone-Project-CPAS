6dd548ffd3dd5f62c6871f0d4a5466a1cd2f933f
amerdilshard786 <amerdilshard786@gmail.com>
2025-10-09 18:15:54 +0800

    Google maps

--- frontend/src/Pages/Contact.js
+++ frontend/src/Pages/Contact.js
@@ -49,6 +49,22 @@ function Contact() {
             </div>
           </div>
 
+          <div className="map-container" style={{ marginTop: "2rem", textAlign: "left" }}>
+            <h2 className="contact-title" style={{ marginBottom: "1rem" }}>
+              Find Us Here
+            </h2>
+            <iframe
+              title="Curtin University Perth Location"
+                src="https://www.google.com/maps/embed?pb=!1m18!1m12!1m3!1d2670.277409285839!2d115.89198247659064!3d-32.00330897399559!2m3!1f0!2f0!3f0!3m2!1i1024!2i768!4f13.1!3m3!1m2!1s0x2a32bc8b6f5e84d1%3A0x987b1b81f6f9a3b5!2s304%2F304%20Brand%20Dr%2C%20Bentley%20WA%206102!5e0!3m2!1sen!2sau!4v1696508654123!5m2!1sen!2sau"
+              width="100%"
+              height="400"
+              style={{ border: 0, borderRadius: "10px" }}
+              allowFullScreen=""
+              loading="lazy"
+              referrerPolicy="no-referrer-when-downgrade"
+            ></iframe>
+          </div> 
+
       </div>
       </div>
     </>
